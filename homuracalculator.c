60933b663ae8bc9158b4183b0506f695e103a996
diegorego514-ship-it <diegorego514@gmail.com>
2025-11-29 22:17:56 -0300

    Add files via upload

---
+++ homuracalculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int operand1 = 5;
+printf("%d", operand1);
+int operand2 = 10;
+printf("%d", operand2);
+int operand3 = 15;
+printf("%d", operand3);
+
+int result;
+printf("%d", result);
+result;
+printf("%d", result);
+result;
+printf("%d", result);
+result;
+printf("%d", result);
+
+int operators;
+printf("%d", operators);
+}
