c1f8b96d8a4427b5f782e9696f6a3246ac91a149
diegorego514-ship-it <diegorego514@gmail.com>
2025-11-30 19:33:07 -0300

    Update homuracalculator to use user input
    Refactor calculator logic to accept user input for operands and operator.

--- homuracalculator.c
+++ homuracalculator.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
+    int x, y, res;
+    int op_num = 0;
+    char op = '\0';
 
-int x = 12;
+    printf("input 1st operand > ");
+    scanf("%d", &x);
+    fseek(stdin, 0, SEEK_END);
+    printf("input 2st operand > ");
+    scanf("%d", &y);
+    fseek(stdin, 0, SEEK_END);
+    printf("enter operator (a number in range 1-4)\n1 => *\n2 => /\n3 => +\n4 => -\n > ");
+    scanf("%d", &op_num);
+    fseek(stdin, 0, SEEK_END);
 
-int y = 7;
+    switch (op_num)
+    {
+    case 1:
+        res = x * y;
+        op = '*';
+        break;
+    case 2:
+        res = x / y;
+        op = '/';
+        break;
+    case 3:
+        res = x + y;
+        op = '+';
+        break;
+    case 4:
+        res = x - y;
+        op = '-';
+        break;
+    default:
+        printf("invalid operator\n");
+        exit(1);
+        break;
+    }
 
-int d;
-
-x = y;
-
-x = 12;
-
-printf("input a number:");
-
-y = 7;
-
-printf("input a number:");
-
-int operator = + 
-
-printf("input a operator:");
+    printf("%d %c %d = %d\n", x, op, y, res);
 }
